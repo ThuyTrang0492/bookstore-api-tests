e67177e2e71a664ac7792acddc7f6a010738787a
CaoThang123 <0306221492@caothang.edu.vn>
2025-07-05 14:19:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -610,7 +610,10 @@ server.use((req, res) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('🚀 Server running at http://localhost:3000');
-}); 
- 
+// server.listen(3000, () => {
+//   console.log('🚀 Server running at http://localhost:3000');
+// }); 
+ const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
